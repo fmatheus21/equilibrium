b4443669da7254eeb1f4f122b249e0768272961d
Fernando Matheus <fernando.matheuss@hotmail.com>
2021-05-07 01:18:23 -0300

    Ajustes

--- src/assets/mail/SMTP.php
+++ src/assets/mail/SMTP.php
@@ -10,7 +10,7 @@ error_reporting(E_ALL);
 
 $mail = new PHPMailer(true);
 
-try {
+    $emailMail        = 'contato@equilibriumracoes.com.br';
 
     $mail->isSMTP(); 
     $mail->Host       = 'smtp.hostinger.com.br';
@@ -21,22 +21,24 @@ try {
     $mail->Port       = 587; 
 
     //Recipients
-    $mail->CharSet = 'UTF-8';
-    $mail->setFrom($email, $name);
-    $mail->addAddress('contato@equilibriumracoes.com.br', $subject); 
-    $mail->addReplyTo($email, $name);  
+    $mail->CharSet    = 'UTF-8';
+    $mail->setFrom($emailMail, $nameMail);
+    $mail->addAddress($emailMail); 
+    $mail->addReplyTo($email);  
 
     //Content
     $mail->isHTML(true);  
-    $mail->Subject = $subject;
-    $mail->Body    = $body;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();   
+    $mail->Subject    = $subjectMail;
+    $mail->Body       = $body;
+    $mail->AltBody    = 'This is the body in plain text for non-HTML mail clients';
     
-    } catch (Exception $e) {
-   
-}
+    try {
+  	$mail->send();       
+        echo 'Email enviado com sucesso!';    
+    } catch (Exception $e) {    
+     $error = 'Mail error: '.$mail->ErrorInfo;    
+      echo $error;   
+   }
 
 
 ?>
