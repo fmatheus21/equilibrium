82d8cbe3bcba8c5bc86f98eb8481ba43077296a2
Fernando Matheus <fernando.matheuss@hotmail.com>
2021-05-09 20:11:01 -0300

    Inclusão de import js

--- src/app/public/slider/slider.component.ts
+++ src/app/public/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScriptService } from 'src/app/script.service';
 
 @Component({
   selector: 'app-slider',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scriptService: ScriptService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    /* Revolution Slider */
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/jquery.themepunch.revolution.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/jquery.themepunch.tools.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.actions.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.carousel.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.kenburn.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.migration.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.navigation.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.parallax.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/plugins/revolution/js/extensions/revolution.extension.video.min.js').then(() => { }).catch(() => { });
+    this.scriptService.loadExternalScript('./assets/js/main-slider-script.js').then(() => { }).catch(() => { });
+
   }
 
 }
